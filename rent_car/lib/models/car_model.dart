5060bdf77ca7b8335282c9176666a44880a81020
MiguelArbelaez0 <119169682+MiguelArbelaez0@users.noreply.github.com>
2023-05-03 02:28:34 -0500

    --

--- rent_car/lib/models/car_model.dart
+++ rent_car/lib/models/car_model.dart
@@ -4,12 +4,12 @@ class Car {
   final String brand;
   final String model;
   final double pricePerDay;
-  final CarDetail carDetail; // Nueva instancia de CarDetail
+  final CarDetail carDetail;
 
   Car({
     required this.brand,
     required this.model,
     required this.pricePerDay,
-    required this.carDetail, // Incluir como parámetro requerido
+    required this.carDetail,
   });
 }
--- rent_car/lib/screens/home/home_scren.dart
+++ rent_car/lib/screens/home/home_scren.dart
@@ -20,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF2F2F2),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
